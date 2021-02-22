bfa32812b2b2d575c95642fdff3a394dde6061aa
Austin Redd <hello@austinredd.com>
2021-02-21 17:42:20 -0700

    adding name to form fields

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -14,17 +14,17 @@ export default () => {
             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="form-name">
             Name
             </label>
-            <input class="appearance-none block w-full bg-night-100 text-gray-700 rounded py-3 px-4 mb-5 leading-tight focus:outline-none" id="form-name" type="text" placeholder="Name"></input>
+            <input class="appearance-none block w-full bg-night-100 text-gray-700 rounded py-3 px-4 mb-5 leading-tight focus:outline-none" id="form-name" type="text" name="name" placeholder="Name"></input>
             
             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="form-email">
             Email
             </label>
-            <input class="appearance-none block w-full bg-night-100 text-gray-700 rounded py-3 px-4 mb-5 leading-tight focus:outline-none" id="form-email" type="text" placeholder="Email Address"></input>
+            <input class="appearance-none block w-full bg-night-100 text-gray-700 rounded py-3 px-4 mb-5 leading-tight focus:outline-none" id="form-email" type="text" name="email" placeholder="Email Address"></input>
             
             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="form-message">
             Message
             </label>
-            <textarea class=" no-resize appearance-none block w-full bg-night-100 text-gray-700 rounded py-3 px-4 mb-5 leading-tight focus:outline-none focus:border-teal-500 h-48 resize-none" id="form-message" placeholder="Enter your message"></textarea>
+            <textarea class=" no-resize appearance-none block w-full bg-night-100 text-gray-700 rounded py-3 px-4 mb-5 leading-tight focus:outline-none focus:border-teal-500 h-48 resize-none" name="message" id="form-message" placeholder="Enter your message"></textarea>
 
             <button class="rounded-full py-3 px-4 uppercase text-xs font-bold cursor-pointer tracking-wider text-teal-500 border-teal-500 border-2 hover:bg-teal-500 hover:text-white transition ease-out duration-700" type="submit">Submit Message</button>
           </form>
