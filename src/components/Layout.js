31e7a713d5c42fdbe04e3ce9f6762615f47b3ce1
Austin Redd <hello@austinredd.com>
2021-02-22 01:37:47 -0700

    fix padding on header

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
     <>
       <SEO />
       <div className="flex flex-col min-h-screen bg-night-200">
-        <header className="py-4 px-10 bg-night-200 text-white font-semibold flex justify-between">
+        <header className="pt-8 pb-4 px-10 bg-night-200 text-white font-semibold flex justify-between">
           <h1 className="max-w-4xl hover:text-teal-500 transition ease-out duration-700">
             <Link to ="/">Austin Redd</Link>
           </h1>
@@ -17,7 +17,7 @@ const Layout = ({ children }) => {
           </nav>
         </header>
         {children}
-        <footer className="py-3 text-center text-gray-700 text-sm">
+        <footer className="pt-6 pb-8 text-center text-gray-700 text-sm">
           &copy; Austin Redd {new Date().getFullYear()}
         </footer>
       </div>
